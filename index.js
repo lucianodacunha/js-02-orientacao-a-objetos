56d7bd961546acb594fbdd450948a35966bf5c54
Luciano <lucianodacunha.net@gmail.com>
2024-09-19 18:31:53 -0300

    Adicionando comportamentos.

--- index.js
+++ index.js
@@ -3,15 +3,51 @@
 class Cliente {
   nome;
   cpf;
-  agencia;
-  saldo;
 }
 
-const c1 = new Cliente();
+class Conta {
+  numero;
+  cliente;
+  _saldo = 0;
 
-c1.nome = "Luciano";
-c1.cpf = "00011122233";
-c1.agencia = 1;
-c1.saldo = 1000.0;
+  sacar (valor){
+    console.log(`Sacando ${valor}.`);
+    if (valor > this._saldo ) {
+      console.log(`Saldo insuficiente.`);
+      return; // early return
+    }
 
-console.log(c1);
+    this._saldo -= valor;    
+  }
+
+  depositar(valor){
+    console.log(`Depositando ${valor}.`);
+    if (valor <= 0){
+      console.log(`Valor incorreto.`);
+      return;
+    }
+    
+    this._saldo += valor;
+
+  }
+}
+
+const cliente = new Cliente();
+
+cliente.nome = "Luciano";
+cliente.cpf = "00011122233";
+
+const conta = new Conta();
+
+conta.numero = 1;
+conta.cliente = cliente;
+
+console.log(conta);
+
+conta.sacar(1100.0);
+console.log(conta);
+
+conta.depositar(100.0);
+conta.depositar(-100.0);
+
+console.log(conta);
